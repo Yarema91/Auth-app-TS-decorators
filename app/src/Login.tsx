7a385e8b44b4499d1d9d442859db9cb4cd1b0788
Yarema <yaremaphoto@gmail.com>
2022-04-27 23:28:28 +0300

    @media + animation

--- app/src/Login.tsx
+++ app/src/Login.tsx
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/style-prop-object */
 /* eslint-disable @typescript-eslint/no-redeclare */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useRef, useState, useEffect, useContext } from 'react';
 import AuthContext from "./context/AuthProvider";
+
+import Lottie from "lottie-react";
+import Animation from "./23640-sign-in-or-sign-up-animation.json";
 
 import axios from './api/axios';
 const LOGIN_URL = '/auth';
@@ -74,34 +79,50 @@ const Login = () => {
                     </p>
                 </section>
             ) : (
-                <section className="frame">
-                    <div className='login'>
+                <section className="frame row">
+                    <Lottie className='animation col-6' animationData={Animation} />
+                    <div className='login col-6'>
 
 
                         <p ref={errRef as any} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
                         <h1>Sign In</h1>
                         <form className="form" onSubmit={handleSubmit}>
-                            <label className="laber" htmlFor="username">Username:</label>
-                            <input
-                                className='input'
-                                type="text"
-                                id="username"
-                                ref={userRef as any}
-                                autoComplete="off"
-                                onChange={(e) => setUser(e.target.value)}
-                                value={user}
-                                required
-                            />
+                            <label className="laber" htmlFor="username">
+                                {/* <i className="bi bi-person-circle"></i> */}
+                                Username:</label>
+                            <div className='inputWithIcon'>
+                                <input
+                                    className='input'
+                                    type="text"
+                                    id="username"
+                                    ref={userRef as any}
+                                    autoComplete="off"
+                                    onChange={(e) => setUser(e.target.value)}
+                                    value={user}
+                                    required
+                                /><span className="bi bi-person-fill"></span>
+                            </div>
 
-                            <label className="laber" htmlFor="password">Password:</label>
-                            <input
-                                className='input'
-                                type="password"
-                                id="password"
-                                onChange={(e) => setPwd(e.target.value)}
-                                value={pwd}
-                                required
-                            />
+                            <label className="laber" htmlFor="password">
+                                {/* <i className="bi bi-lock-fill"></i> */}
+                                Password:</label>
+                            {/* <i className="bi bi-eye-slash-fill"></i> */}
+                            <div className='inputWithIcon'>
+                                <input
+                                    className='input'
+                                    type="password"
+                                    id="password"
+                                    onChange={(e) => setPwd(e.target.value)}
+                                    value={pwd}
+                                    required
+                                /><i className="bi bi-lock-fill"></i>
+                                <a href="#" className='hide-code'>
+                                    <i className="bi bi-eye-slash-fill"></i>
+                                    {/* <i className="bi bi-eye-fill"></i> */}
+                                </a>
+
+                            </div>
+
                             <button className='button'>Sign In</button>
                         </form>
                         <p className="sign-up">
@@ -110,7 +131,9 @@ const Login = () => {
                                 {/*put router link here*/}
                                 <a href="#">Sign Up</a>
                             </span>
+
                         </p>
+
                     </div>
                 </section>
             )}
